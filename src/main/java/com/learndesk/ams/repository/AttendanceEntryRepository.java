d36d985a20d07e91467c6e4e4b0aed9884a00ad8
Sai Krishna <saikrishna.mallupattu@hidglobal.com>
2019-10-14 07:44:09 +0530

    attendance entity updates

--- src/main/java/com/learndesk/ams/repository/AttendanceEntryRepository.java
+++ src/main/java/com/learndesk/ams/repository/AttendanceEntryRepository.java
@@ -1,8 +1,12 @@
 package com.learndesk.ams.repository;
 
 import com.learndesk.ams.domain.AttendanceEntry;
+import com.learndesk.ams.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 
 
 /**
@@ -11,5 +15,7 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface AttendanceEntryRepository extends JpaRepository<AttendanceEntry, Long> {
+
+    Page<AttendanceEntry> findAllByUser(User user, Pageable pageable);
 
 }
